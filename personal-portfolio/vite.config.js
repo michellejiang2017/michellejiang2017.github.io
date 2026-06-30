975d7b6d0a72f1571cd2b3f6dfe8bbf289257a6b
Michelle Jiang <mhjiang@smith.edu>
2026-06-30 11:56:45 -0400

    Update vite.config.js

--- personal-portfolio/vite.config.js
+++ personal-portfolio/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Portfolio/', // Ensure this matches your repository name
+  base: '/', // Ensure this matches your repository name
 })
